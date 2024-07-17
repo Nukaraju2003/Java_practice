3b5cb68ea48f4d9446a2d3cbb6d1190bac53c526
NUKARAJU NERADABILLI <neradabillinukaraju@gmail.com>
2024-07-17 10:34:55 +0530

    character

--- Javaw3schools/JavaExamples/Syntax.java
+++ Javaw3schools/JavaExamples/Syntax.java
@@ -1,6 +1,19 @@
-public class Syntax {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
+// public class Syntax {
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
     
+// }
+
+
+
+public class Syntax{
+    public static void main(String[] args){
+        // System.out.println("Hello world");
+        int mynum;
+        mynum = 15;
+        String name = "John";
+        System.out.println(mynum);
+
+    }
 }
---
+++ Javaw3schools/JavaExamples/character.java
@@ -0,0 +1,10 @@
+public class character {
+    public static void main(String[] args){
+        char myvar1 = 65, myvar2 = 66, myvar3 = 76;
+        System.out.println(myvar1);
+        System.out.println(myvar2);
+        System.out.println(myvar3);
+        
+    }
+    
+}
